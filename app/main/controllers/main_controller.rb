a18f847364d249d877f3b9f955f779d537614d1d
elliotthilaire <elliott.hilaire@gmail.com>
2015-10-23 08:29:07 +1000

    add progress bar and counter

--- app/main/controllers/main_controller.rb
+++ app/main/controllers/main_controller.rb
@@ -20,6 +20,32 @@ module Main
       _todos[(params._index || 0).to_i]
     end
 
+    def check_all
+      _todos.each { |todo| todo._completed = true }
+    end
+
+    def completed
+      _todos.count { |t| t._completed }
+    end
+
+    def incomplete
+    # because .size and completed both return promises, we need to
+    # call .then on them to get their value.
+      _todos.size.then do |size|
+        completed.then do |completed|
+          size - completed
+        end
+      end
+    end
+
+    def percent_complete
+      _todos.size.then do |size|
+        completed.then do |completed|
+          (completed / size.to_f * 100).round
+        end
+      end
+    end
+
     private
 
     # The main template contains a #template binding that shows another
